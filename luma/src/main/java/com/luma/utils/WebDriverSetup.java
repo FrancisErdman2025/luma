4a359478330cf9bd14bcd6383105a7cefe66932e
FrancisErdman2025 <franciserdman@mailfence.com>
2025-02-07 06:52:14 -0500

    Update WebDriverSetup.java
    refactor to support FF also in addition to chrome by default

--- luma/src/main/java/com/luma/utils/WebDriverSetup.java
+++ luma/src/main/java/com/luma/utils/WebDriverSetup.java
@@ -2,13 +2,39 @@ package com.luma.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class WebDriverSetup {
     public static WebDriver initializeDriver() {
-    	WebDriverManager.chromedriver().clearDriverCache().setup(); // Clears cache and updates driver
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize(); // Maximizes the browser window
+        Properties props = new Properties();
+        try (InputStream input = WebDriverSetup.class.getResourceAsStream("/config.properties")) {
+            if (input != null) {
+                props.load(input);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        String browser = props.getProperty("browser", "chrome").toLowerCase();
+
+        WebDriver driver;
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().clearDriverCache().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().clearDriverCache().setup();
+                driver = new ChromeDriver();
+                break;
+        }
+
+        driver.manage().window().maximize();
         return driver;
     }
 }
