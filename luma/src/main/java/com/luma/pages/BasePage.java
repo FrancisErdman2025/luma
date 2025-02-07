285ff8dac6e51e3ef0d96a3ee341a4901baa14de
FrancisErdman2025 <franciserdman@mailfence.com>
2025-02-07 06:29:57 -0500

    Update BasePage.java
    refactor

--- luma/src/main/java/com/luma/pages/BasePage.java
+++ luma/src/main/java/com/luma/pages/BasePage.java
@@ -1,7 +1,9 @@
 package com.luma.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
@@ -13,5 +15,31 @@ public class BasePage {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         PageFactory.initElements(driver, this);
+    }
+    
+    protected void clickOnElement(WebElement webElement) {
+    	this.wait.until(ExpectedConditions.elementToBeClickable(webElement)).click();
+    }
+    
+    protected void clearElement(WebElement webElement) {
+    	this.wait.until(ExpectedConditions.elementToBeClickable(webElement)).clear();
+    }
+    
+    protected void sendKeysToElement(WebElement webElement, String sInput) {
+    	this.wait.until(ExpectedConditions.elementToBeClickable(webElement)).sendKeys(sInput);
+    }
+    
+    protected boolean isElementDisplayed(WebElement webElement) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            wait.until(ExpectedConditions.visibilityOf(webElement));
+            return webElement.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    protected String getElementText(WebElement webElement) {
+        return wait.until(ExpectedConditions.visibilityOf(webElement)).getText();
     }
 }
